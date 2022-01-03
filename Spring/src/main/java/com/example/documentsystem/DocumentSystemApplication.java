2df8fc4ffde0206da5ee6d0ee557ad85c15ee130
Vasil Papukchiev <33736545+vasilp98@users.noreply.github.com>
2022-01-03 23:50:04 +0200

    Add SpringBoot application

---
+++ Spring/src/main/java/com/example/documentsystem/DocumentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.documentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentSystemApplication.class, args);
+    }
+
+}
---
+++ Spring/src/test/java/com/example/documentsystem/DocumentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
